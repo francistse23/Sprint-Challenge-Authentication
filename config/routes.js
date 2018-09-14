18b9e02b1dea9bca54957c34f5ddc876bd1a4362
francistse23 <wtse.ucsd@gmail.com>
2018-09-14 08:36:52 -0700

    Login method implemented. Successful login will return a token, which the user can put in the header to get access to the jokes page. User key/value pair Authorization/(token)

--- config/routes.js
+++ config/routes.js
@@ -46,8 +46,22 @@ function register(req, res) {
 }
 
 function login(req, res) {
-  // implement user login
-}
+  const creds = req.body;
+  db('users')
+    .where({ username: creds.username})
+    .first()
+    .then( user => {
+      if (user && bcrypt.compareSync( creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({
+          message: "Invalid authentication. Please try again."
+        });
+      }
+    })
+    .catch( err => res.status(500).json( err.message ));
+};
 
 function getJokes(req, res) {
   axios
